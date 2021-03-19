64537f864aa3be6cfe7905b30bfb03d1ecba404c
hoodieman0 <hoodieman8@gmail.com>
2021-03-19 03:02:43 -0400

    Update 2
    Created GameController.* 
    Created SlotMachine.hpp, the place for three reels to gather. 
    Updated Circular LL to use structs for Nodes, booleans to check if the list is empty, added debugging outputs (this part gives segmentation faults supposedly, in the form of stackdumps).
    Updated Makefile to delete stackdumps, rename build to program.exe.
    Updated main.cpp for better tests.
    Still have to figure out how to fix those stackdumps; most likely caused by lines 70 and 84 in CircularLinkedListTemp.hpp.
    Might remove unnecessary function MakeEmpty in CircularLinkedListTemp.hpp.

--- CSCI-2226/main.cpp
+++ CSCI-2226/main.cpp
@@ -8,31 +8,56 @@ Date Created:: 9 March 2021
 Objective:: Successfully use templates and circular linked lists.
 */
 
+#include "SlotMachine.hpp"
 #include "CircularLinkedListTemp.hpp"
 
 #include <iostream>
 
 int main()
 {
-    cout << "Gacha Ruins Your Life" << endl;
+    std::cout << "~Gacha Ruins Your Life~" << endl;
     
-    LinkedList<char> reelOne;
-    reelOne.Intitialize();
-    reelOne.PutItem('a');
-    reelOne.PutItem('a');
-    reelOne.PutItem('a');
-    reelOne.PutItem('a');
-    reelOne.PutItem('b');
-    reelOne.PutItem('b');
-    reelOne.PutItem('b');
-    reelOne.PutItem('c');
-    reelOne.PutItem('c');
-    reelOne.PutItem('7');
+    char arrayOne[10] = {'a','a','a','a','b','b','b','c','c','7'};
+    char arrayTwo[10] = {'c','c','b','a','b','a','7','a','a','b'};
+    char arrayThree[10] = {'b','a','b','a','b','c','a','a','c','7'};
+
+    // SlotMachine<char>* gachaMachine;
+    // std::cerr << "Creation Passed" << endl;
+    // gachaMachine->Initialize(arrayOne, arrayTwo, arrayThree);
+    // std::cerr << "Initialize Passed" << endl;
+    // gachaMachine->Debug();
+    // std::cerr << "Debug Passed" << endl;
+    // std:: cerr << gachaMachine->Spin(gachaMachine->GetReelOne(), gachaMachine->GetReelTwo(), gachaMachine->GetReelThree());
+    // std::cerr << "Spin Passed" <<endl;
+
+    LinkedList<char>* reelOne;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        reelOne->PutItem(arrayOne[i]);
+        std::cout << "Putting " << arrayOne[i] << endl;
+    }
 
     for (int i = 0; i < 10; i++)
     {
-        std::cout << reelOne.GetNextItem();
+        std::cout << reelOne->GetNextItem();
     }
 
-    cout << "~End of Program~"
+    // char input;
+    // bool loop = true;
+    // GameController Headhunting;
+
+    // std::cout << "Spin the Gacha? (Y/N)" << endl;
+    // cin >> input;
+
+    // if (std::toupper(input) == 'Y')
+    // {
+    //     Headhunting.PlayGame(100);
+    // }
+    // else
+    // {
+    //     std::cout << "No Dice" << endl;
+    // }
+
+    std::cout << "\n~End of Program~";
 }
--- CSCI-2226/makefile
+++ CSCI-2226/makefile
@@ -1,11 +1,18 @@
 CC=g++
 CFLAGS=-std=c++11
-binaries = main.o 
+binaries = main.o GameController.o
 
-main: $(binaries)
+all: program
 
-main.o: main.cpp
+program: $(binaries)
+	$(CC) $(binaries) -o program
+
+main.o: main.cpp CircularLinkedListTemp.hpp SlotMachine.hpp
 	$(CC) -c $(CFLAGS) main.cpp
 
+GameController.o: GameController.cpp GameController.hpp
+	$(CC) -c $(CFLAGS) GameController.cpp
+
 clean: 
-	rm main $(binaries)
+	rm program $(binaries)
+	rm -f *.stackdump
