95d2a9b3a9f38273824c20a8280cd850b83321c7
hoodieman0 <hoodieman8@gmail.com>
2021-03-20 00:35:37 -0400

    Update 3
    Got it to work by using Node<T>* variable = new Node<T>;
    I hate how that was too easy...

--- CSCI-2226/CircularLinkedListTemp.hpp
+++ CSCI-2226/CircularLinkedListTemp.hpp
@@ -9,7 +9,7 @@ using namespace std;
 template <class T> struct Node
 {
     T data;
-    struct Node* next;
+     Node* next;
 };
 
 template <class T> class LinkedList
@@ -17,90 +17,68 @@ template <class T> class LinkedList
     private:
         int length;
 
-        struct Node<T>* currentPosition = nullptr;
-        struct Node<T>* head = nullptr;
-        struct Node<T>* back = nullptr;
-
-        bool isEmpty = true;
-        bool isCurrentPosition = true;
+        Node<T>* currentPosition;
+        Node<T>* back;
 
     public:
-        LinkedList();
-        ~LinkedList();
-        void MakeEmpty();
-        void PutItem(T);
-        T GetNextItem();
-};
+        LinkedList()
+        {
+            length = 0;
+            currentPosition = nullptr;
+            back = nullptr;
+        }
 
+        ~LinkedList()
+        {
+            MakeEmpty();
+        }
 
-template <class T> LinkedList<T>::LinkedList()
-{
-    length = 0;
-    currentPosition = nullptr;
-    head = nullptr;
-    back = nullptr;
-}
+        void MakeEmpty()
+        {
+            struct Node<T>* temp = nullptr;
+            while(back != nullptr)
+            {
+                temp = back;
+                back = back->next;
+                delete temp;
+            }
+            length = 0;
+        }
 
-template <class T> LinkedList<T>::~LinkedList()
-{
-    MakeEmpty();
-}
+        void PutItem(T symbol)
+        {
+            Node<T>* temp = new Node<T>;
+	        temp->data = symbol;
+            if(back == nullptr)
+            {
+                back = temp;
+                temp->next = back;
+            } 
+	        else
+            {
+                temp->next = back->next;
+                back->next = temp;
+            }
 
-template <class T> void LinkedList<T>::MakeEmpty()
-{
-    struct Node<T>* temp = nullptr;
-    while(head != nullptr)
-    {
-        temp = head;
-        head = head->next;
-        delete temp;
-    }
-    length = 0;
-}
+	        length++; 
+        }
 
-template <class T> void LinkedList<T>::PutItem(T symbol)
-{
-    struct Node<T>* temp;
-	temp->data = symbol;
-    std::cerr << "Assignments Passed" << endl;
+        T GetNextItem()
+        {
+            Node<T>* temp = new Node<T>;
+            temp = back;
     
-    if(isEmpty) //this would not trigger with back == nullptr
-    {
-        std::cerr << "2Passed" << endl;
-        back = temp;
-        std::cerr << "Passed" << endl;
-        back->next = back;
-        isEmpty = false;
-        std::cerr << "Back Creation Passed" << endl;
-    } 
-	else
-    {
-        std::cerr << "3Passed" << endl;
-        temp->next = back->next;
-        back->next = temp;
-        std::cerr << "Insertion Passed" << endl;
-    }
-
-    head = temp;
-    std::cerr << "Head Creation Passed" << endl;
-
-	length++; 
-}
-
-template <class T> T LinkedList<T>::GetNextItem()
-{
-    if (isCurrentPosition) //does not trigger with currentPosition == nullptr
-    {
-        currentPosition = head;
-        isCurrentPosition = false;
-    }
-    else
-    {
-        currentPosition = currentPosition->next;
-    }
-	std::cerr << head->data << "|" << currentPosition->data << endl;
-    T stuff = currentPosition->data;
-	return stuff;
-}
+            if (currentPosition == nullptr)
+            {
+                currentPosition = temp;
+            }
+            else
+            {
+                currentPosition = currentPosition->next;
+            }
+            T stuff = currentPosition->data;
+	        return stuff; 
+        }
+};
 
 #endif
