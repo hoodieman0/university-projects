7b22f304ae160ccb7077292bdb98afef90ae41c1
hoodieman0 <hoodieman8@gmail.com>
2021-03-20 00:36:21 -0400

    Debug Function
    This iteration has the debug function for SlotMachine.hpp

--- CSCI-2226/SlotMachine.hpp
+++ CSCI-2226/SlotMachine.hpp
@@ -6,116 +6,107 @@
 #include <iostream>
 #include <ctime>
 
+#define MAX_SIZE 10
+
 template <class T> class SlotMachine
 {
     private:
-        LinkedList<T>* reelOne;
-        LinkedList<T>* reelTwo;
-        LinkedList<T>* reelThree;
+        LinkedList<T>* reelOne = new LinkedList<T>;
+        LinkedList<T>* reelTwo = new LinkedList<T>;
+        LinkedList<T>* reelThree = new LinkedList<T>;
 
     public:
-        void Initialize(T [], T[], T[]);
-        LinkedList<T>* GetReelOne();
-        LinkedList<T>* GetReelTwo();
-        LinkedList<T>* GetReelThree();
-        std::string Spin(LinkedList<T>*, LinkedList<T>*, LinkedList<T>*);
+        SlotMachine(T (&arrayOne)[MAX_SIZE], T (&arrayTwo)[MAX_SIZE], T (&arrayThree)[MAX_SIZE])
+        {
+            for (auto &data : arrayOne)
+            {
+                reelOne->PutItem(data);
+            }
+    
+            for (auto &data : arrayTwo)
+            {
+                reelTwo->PutItem(data);
+            }
+    
+            for (auto &data : arrayThree)
+            {
+                reelThree->PutItem(data);
+            }
+        }
+
+        LinkedList<T>* GetReelOne()
+        {
+            return reelOne;
+        }
+
+        LinkedList<T>* GetReelTwo()
+        {
+            return reelTwo;
+        }
+
+        LinkedList<T>* GetReelThree()
+        {
+            return reelThree;
+        }
+
+        string Spin(LinkedList<T>* first, LinkedList<T>* second, LinkedList<T>* third)
+        {
+            T symbolOne; //LinkedList and SlotMachine must be same data type for this to work
+            T symbolTwo;
+            T symbolThree;
+
+            std::string finalCombo;
+
+            srand((unsigned) time(0)); 
+            int spinOne = (rand() % 100) + 1;
+
+            srand((unsigned) time(0) - 393);
+            int spinTwo = (rand() % 100) + 1;
+
+            srand((unsigned) time(0) + 255);
+            int spinThree = (rand() % 100) + 1;
+
+            std::cout << "|" << spinOne << "|" << spinTwo << "|" << spinThree << "|" << endl;
+
+            for (int i = 0; i < spinOne; i++)
+            {
+                symbolOne = first->GetNextItem();
+            }
+
+            for (int i = 0; i < spinTwo; i++)
+            {
+                symbolTwo = second->GetNextItem();
+            }
+
+            for (int i = 0; i < spinThree; i++)
+            {
+                symbolThree = third->GetNextItem();
+            }
+
+            finalCombo = finalCombo + symbolOne + symbolTwo + symbolThree;
+            std::cout << finalCombo << endl;
+
+            return finalCombo;
+        }
         
-        void Debug();
+        void Debug()
+        {
+            std::cerr << "Debug Call:" << endl;
+            for (int i =0; i < MAX_SIZE; i++)
+            {
+                std::cout << reelOne->GetNextItem();
+            }
+            std::cout << endl;
+            for (int i =0; i < MAX_SIZE; i++)
+            {
+                std::cout << reelTwo->GetNextItem();
+            }
+            std::cout << endl;
+            for (int i =0; i < MAX_SIZE; i++)
+            {
+                std::cout << reelThree->GetNextItem();
+            }
+        }
 };
-
-template <class T> void SlotMachine<T>::Initialize(T arrayOne[], T arrayTwo[], T arrayThree[]) //make range based for loop?
-{
-    
-    for (int i = 0; i < 10; i++)
-    {
-        reelOne->PutItem(arrayOne[i]);
-    }
-
-    // for (int i = 0; i < (sizeof(arrayTwo)/sizeof(arrayTwo[0]); i++)
-    // {
-    //     reelTwo->PutItem(arrayTwo[i]);
-    // }
-
-    // for (int i = 0; i < (sizeof(arrayThree)/sizeof(arrayThree[0])); i++)
-    // {
-    //     reelThree->PutItem(arrayThree[i]);
-    // }
-    
-    // for (auto i : arrayTwo)
-    // {
-    //     reelTwo->PutItem(arrayTwo[i]);
-    // }
-    
-    // for (auto i : arrayThree)
-    // {
-    //     reelThree->PutItem(arrayThree[i]);
-    // }
-}
-
-template <class T> LinkedList<T>* SlotMachine<T>::GetReelOne()
-{
-    return reelOne;
-}
-
-template <class T> LinkedList<T>* SlotMachine<T>::GetReelTwo()
-{
-    return reelTwo;
-}
-
-template <class T> LinkedList<T>* SlotMachine<T>::GetReelThree()
-{
-    return reelThree;
-}
-
-template <class T> std::string SlotMachine<T>::Spin(LinkedList<T>* first, LinkedList<T>* second, LinkedList<T>* third)
-{
-    T symbolOne; //LinkedList and SlotMachine must be same data type for this to work
-    T symbolTwo;
-    T symbolThree;
-
-    string finalCombo;
-    
-
-    srand(time(0)); 
-    int spinOne = (rand() % 100) + 1;
-
-    srand(time(0));
-    int spinTwo = (rand() % 100) + 1;
-
-    srand(time(0));
-    int spinThree = (rand() % 100) + 1;
-
-    // std::cout << "|" << spinOne << "|" << spinTwo << "|" << spinThree << "|" << endl;
-
-    for (int i = 0; i < spinOne; i++)
-    {
-        symbolOne = first->GetNextItem();
-    }
-
-    // for (int i = 0; i < spinTwo; i++)
-    // {
-    //     symbolTwo = second->GetNextItem();
-    // }
-
-    // for (int i = 0; i < spinThree; i++)
-    // {
-    //     symbolThree = third->GetNextItem();
-    // }
-
-    finalCombo = symbolOne + symbolTwo + symbolThree;
-    std::cout << finalCombo << endl;
-
-    return finalCombo;
-}
-
-template <class T> void SlotMachine<T>::Debug()
-{
-    std::cerr << "Debug Call:" << endl;
-    for (int i =0; i < 10; i++)
-    {
-        std::cout << reelOne->GetNextItem();
-    }
-}
-
 
 #endif
