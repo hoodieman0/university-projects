d2802c7e9d9d3b2223775c70690c4dd7f1427c17
James Mok <hoodieman8@gmail.com>
2022-02-21 09:10:41 -0500

    main file TODO
    all the stuff I have currently learned but have to implement

---
+++ CSCI-4478-Algorithms/main.py
@@ -0,0 +1,17 @@
+# TODO Nodes
+# TODO Trees
+# TODO Graphs
+# TODO BFS
+# TODO DFS Limited To Unlimited
+# TODO UCS
+# TODO Bidirectional Search
+# TODO Greedy Best-First Search
+# TODO Dijkstra's Algorithm
+# TODO A* Search
+# TODO MINIMAX Algorithm
+# TODO Alpha-Beta Pruning
+# TODO Time and Space Complexity
+
+# b is branching factor
+# m is maximum depth
+# d is depth of shallowest goal
