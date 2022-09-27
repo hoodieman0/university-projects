3aecc8ecd31e18892ee1950f80cf58a8120f954b
James Mok <hoodieman8@gmail.com>
2022-09-26 22:19:51 -0400

    changed regex to string.find()

--- CSCI-4526-Sudoku/P4-KorideMok/Square-KorideMok.cpp
+++ CSCI-4526-Sudoku/P4-KorideMok/Square-KorideMok.cpp
@@ -1,7 +1,6 @@
 // Written by James Mok and Neelakanta Bharadwaj Koride
 
 #include "Square-KorideMok.hpp"
-#include <regex> //used in mark
 
 // ---------------------------------------------------------------------
 // Prints the values of the Square object in a human-readable format
@@ -15,9 +14,8 @@ Square(char input, short r, short c) : info(input), row(r), col(c){
 void Square::
 mark(char marker){
     string temp(1,marker);
-    regex reg ("[1-9]");
-    smatch result;
-    if(regex_search(temp, result, reg)) {
+    string valid = "123456789";
+    if(valid.find(marker)) {
         info.mark(marker);
     }
     else{ cout <<"Invalid Input" <<endl; }
