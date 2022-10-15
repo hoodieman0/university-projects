1c197f5fa75296804f3b3ff2e9a881da557578dc
James Mok <hoodieman8@gmail.com>
2022-10-14 21:15:33 -0400

    Updated P3 to fix constructor

--- CSCI-4526-Sudoku/P3-KorideMok/Game-KorideMok.hpp
+++ CSCI-4526-Sudoku/P3-KorideMok/Game-KorideMok.hpp
@@ -1,3 +1,4 @@
+// Written by James Mok and Neelakanta Bharadwaj Koride
 
 #ifndef GAME_HPP
 #define GAME_HPP
@@ -8,14 +9,15 @@ class Board;
 
 class Game{
     private:
-        const static char* menu[6];
+        const static string menu[6];
         Board* puzzle;
         int n = 9;
         char gameType = 't';
         ifstream file;
 
     public:
-        Game(string);
+        Game(char*);
+        ~Game() { delete puzzle; }
         void run();
 };
 
