ddfede6765c4fb879df80d0c321144a164b9b2d2
James Mok <hoodieman8@gmail.com>
2022-10-16 00:40:07 -0400

    created unit test wrappers for each project

--- CSCI-4526-Sudoku/P5-KorideMok/SudokuMain-KorideMok.cpp
+++ CSCI-4526-Sudoku/P5-KorideMok/SudokuMain-KorideMok.cpp
@@ -6,7 +6,7 @@
 // if file output is wanted, use 'unit_test' variable
 
 // argv[1] is the file name used in Board class
-int main(int argc, char * const argv[]){
+int main(int argc, char* const argv[]){
     banner();
 
     if (argc != 2){ fatal("!Incorrect Amount Of Arguments!"); }
--- CSCI-4526-Sudoku/P5-KorideMok/UnitTests-KorideMok.hpp
+++ CSCI-4526-Sudoku/P5-KorideMok/UnitTests-KorideMok.hpp
@@ -14,17 +14,46 @@ void testSquareFunctions(ostream&); //P2
 void testBoardFunctions(ostream&, char*); //P4
 void testClusterFunctions(ostream&, char*); //P5
 
-inline void testP1(ostream& out) { testSquareFunctions(out); }
-inline void testP2(ostream& out) {
+void
+testP1(ostream& out) {
+    out <<"~Begin State Test~\n---------------------------------------------------" <<endl;
     testSquareFunctions(out);
-    out <<"-----------------------------------------------------"
-          "-----------------------------------------------------" <<endl;
-    testStateFunctions(out);
+    out <<"\n~State Test Completed Successfully~\n-----------------------------------------"
+          "----------" <<endl;
 }
 
-inline void testP3(ostream& out) { }
-inline void testP4(ostream& out) { }
-inline void testP5(ostream& out, char* filename) {
+void
+testP2(ostream& out) {
+    out <<"~Begin Square Test~\n---------------------------------------------------" <<endl;
+    testSquareFunctions(out);
+    out <<"\n~Square Test Completed Successfully~\n-----------------------------------------"
+          "----------" <<endl;
+
+    out <<"~Begin State Test~\n---------------------------------------------------" <<endl;
+    testStateFunctions(out);
+    out <<"\n~State Test Completed Successfully~\n-----------------------------------------"
+          "----------" <<endl;
+}
+
+void
+testP3(ostream& out, const char* filename) {
+    out <<"~Begin Game Test~\n---------------------------------------------------" <<endl;
+    Game obj(filename);
+    obj.run();
+    out <<"\n~Game Test Completed Successfully~\n-----------------------------------------"
+          "----------" <<endl;
+}
+
+void
+testP4(ostream& out, char* filename) {
+    out <<"~Begin Board Test~\n---------------------------------------------------" <<endl;
+    testBoardFunctions(out, filename);
+    out <<"\n~Board Test Completed Successfully~\n-----------------------------------------"
+          "----------" <<endl;
+}
+
+void
+testP5(ostream& out, char* filename) {
     out <<"~Begin Cluster Test~\n---------------------------------------------------" <<endl;
     testClusterFunctions(out, filename);
     out <<"\n~Cluster Test Completed Successfully~\n-----------------------------------------"
