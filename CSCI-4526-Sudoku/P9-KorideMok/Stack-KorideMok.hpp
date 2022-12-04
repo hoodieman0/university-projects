7bbd544fcc34590843d6bb2df4eb575e2a4c34af
James Mok <hoodieman8@gmail.com>
2022-12-04 02:24:24 -0500

    created Stack class, added to makefile

---
+++ CSCI-4526-Sudoku/P9-KorideMok/Stack-KorideMok.hpp
@@ -0,0 +1,21 @@
+// Written by James Mok
+
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "tools.hpp"
+#include "Frame-KorideMok.hpp"
+
+class Stack : private vector<Frame*>{
+    public:
+        Stack()=default;
+        ~Stack()=default;
+        void pop() { vector::pop_back(); }
+        Frame* top() { return vector::back(); }
+        void push(Frame* f) { vector::push_back(f); }
+        int size() { return vector::size(); }
+        void zap() { while (!vector::empty()) vector::pop_back(); }
+
+};
+
+#endif
