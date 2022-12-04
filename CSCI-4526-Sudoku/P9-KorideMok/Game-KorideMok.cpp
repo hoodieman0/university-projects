23d2c0fc73e852713c44d08eebb12f40a36fad19
James Mok <hoodieman8@gmail.com>
2022-12-04 04:42:10 -0500

    added Redo to Game::run()

--- CSCI-4526-Sudoku/P9-KorideMok/Game-KorideMok.cpp
+++ CSCI-4526-Sudoku/P9-KorideMok/Game-KorideMok.cpp
@@ -89,10 +89,20 @@ run(){
                         puzzle->sub(row+1, col+1).setState(frame->getState(row+col));
                     }
                 }
-
                 continue;
 
             case 'R': continue;
+                if (redo.size() == 0) { cout <<"No Moves To Redo!" <<endl; continue; }
+                frame = redo.top();
+                redo.pop();
+                undo.push(frame);
+
+                for (int row = 0; row < n; row++){
+                    for (int col = 0; col < n; col++){
+                        puzzle->sub(row+1, col+1).setState(frame->getState(row+col));
+                    }
+                }
+                continue;
 
             case 'S': continue;
 
