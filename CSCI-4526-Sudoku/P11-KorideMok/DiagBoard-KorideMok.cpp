fe2825c2e44f6ffd732736a683a4ed47c7c6868b
James Mok <hoodieman8@gmail.com>
2022-12-14 17:33:12 -0500

    initialized P11 from P10, added pdf

---
+++ CSCI-4526-Sudoku/P11-KorideMok/DiagBoard-KorideMok.cpp
@@ -0,0 +1,33 @@
+// Written by James Mok
+
+#include "DiagBoard-KorideMok.hpp"
+
+static const string clusterT[4] = {"ROW", "COLUMN", "BOX", "DIAGONAL"};
+
+// ---------------------------------------------------------------------
+// Constructor for DiagBoard
+// Preconditions: Game object exists
+// Postconditions: creates a DiagBoard object
+DiagBoard::
+DiagBoard(short n, short clstr, ifstream& file) : Board(n, clstr, file) {
+    //Is knowing the amount of clusters important if I am making a diagonal board?
+    createDiagonal();
+}
+
+// ---------------------------------------------------------------------
+// Creates the diagonal clusters for a diagonal board
+// Preconditions: Board object exists
+// Postconditions: adds two diagonal clusters to buddies
+void DiagBoard::
+createDiagonal() {
+    Square* topArr[9];
+    Square* botArr[9];
+    for (int row = 0; row < 9; row++) {
+        topArr[row] = &sub(row+1, row+1);
+        botArr[row] = &sub(row+1, 9-row);
+    }
+    Cluster* tempOne = new Cluster(clusterT[(int)ClusterType::DIAGONAL], topArr);
+    Cluster* tempTwo = new Cluster(clusterT[(int)ClusterType::DIAGONAL], botArr);
+    Board::buddies.push_back(tempOne);
+    Board::buddies.push_back(tempTwo);
+}
---
+++ CSCI-4526-Sudoku/P11-KorideMok/Frame-KorideMok.hpp
@@ -0,0 +1,27 @@
+// Written by James Mok
+
+#ifndef FRAME_HPP
+#define FRAME_HPP
+
+#include "tools.hpp"
+#include "State-KorideMok.hpp"
+
+class Frame{
+    private:
+        State arr[81];
+
+    public:
+        Frame()=default;
+        explicit Frame(State input[81]);
+        Frame& operator=(const Frame&)=default; //copy assignments 
+        Frame(const Frame&) = default; 
+        Frame& operator=(const Frame&&)=delete; //remove move assignments. 
+        Frame(const Frame&&) = delete; 
+        ~Frame()=default;
+        State getState(int index) { return arr[index]; }
+        void serialize(ofstream& outFile, char gameType); 
+        void realize(ifstream& inFile); 
+
+};
+
+#endif
