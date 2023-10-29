e097fd5e22e125acbc601096201376d87ea4a10b
James Mok <hoodieman8@gmail.com>
2023-10-29 00:25:03 -0400

    fix: Types.hpp fixed multiple redefinition of TokenTypeName array

--- CSCI-4536/Lab3/Types.hpp
+++ CSCI-4536/Lab3/Types.hpp
@@ -8,6 +8,6 @@ using namespace std;
 enum TokenType {WORD, NUMBER, STRING};
 enum StateType {STARTING, DONE, ACQUIRING_TOKEN, SLASH_PENDING, ACQUIRING_SLASH, PAREN_PENDING, ACQUIRING_PAREN, ACQUIRING_STRING};
 
-string TokenTypeName[4] = {"Word", "Number", "String"};
+const string TokenTypeName[4] = {"Word", "Number", "String"};
 
 #endif
