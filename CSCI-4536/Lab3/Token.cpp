d1a25aeebcc8a29bcc452252ed79d1333da50e12
James Mok <hoodieman8@gmail.com>
2023-10-28 22:05:20 -0400

    feat: fully implemented Token class

--- CSCI-4536/Lab3/Token.cpp
+++ CSCI-4536/Lab3/Token.cpp
@@ -1,4 +1,17 @@
 #include "Token.hpp"
+
+Token::
+Token(string name, TokenType type) : name(name), type(type), refCount(1) {
+}
+
 ostream& Token::
 print(ostream& out){
+    out << "Token Name: " << name << " | Type: " << TokenTypeName[type] << " | Times Referenced: " << refCount;
+    return out;
 }
+    
+    
+void Token::
+incrementRefCount() {
+    refCount++;
+};
