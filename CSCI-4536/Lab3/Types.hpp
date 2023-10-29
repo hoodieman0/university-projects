855e58c34282b353182b610cae5ee494e84ba8e4
James Mok <hoodieman8@gmail.com>
2023-10-28 21:58:56 -0400

    feat: added string array to types.hpp for ease of conversion to string

--- CSCI-4536/Lab3/Types.hpp
+++ CSCI-4536/Lab3/Types.hpp
@@ -1,2 +1,13 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <string>
+
+using namespace std;
+
 enum TokenType {WORD, NUMBER, STRING};
 enum StateType {STARTING, DONE, ACQUIRINGTOKEN, ACQUIRINGCOMMENT, ACQUIRINGSTRING};
+
+string TokenTypeName[3] = {"Word", "Number", "String"};
+
+#endif
