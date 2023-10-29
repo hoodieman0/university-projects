411dddfe29f4adaaaff08830eac03ce4bd9a6fa5
James Mok <hoodieman8@gmail.com>
2023-10-28 22:23:19 -0400

    feat: added constructor and destructor for Lexer class

--- CSCI-4536/Lab3/Lexer.cpp
+++ CSCI-4536/Lab3/Lexer.cpp
@@ -1,7 +1,22 @@
 #include "Lexer.hpp"
 
-Lexer::Lexer(string filename);
-Lexer::~Lexer();
+Lexer::Lexer(string filename){
+    inFile.open(filename);
+    if (!inFile) throw "Error: Could not open input file!";
+
+    outFile.open(filename + ".out");
+    if (!outFile) throw "Error: Could not open output file!";
+
+    outFile << "James Mok, Andrew Haller, Anusha Mandadam" << endl;
+    outFile << "Lab 3: Forth Lexer" << endl;
+    outFile << "Lexed File: " << filename << endl;
+    outFile << "Comments: " << endl;
+}
+
+Lexer::~Lexer(){
+    inFile.close();
+    outFile.close();
+}
 
 void Lexer::doLex();
 ostream& Lexer::print(ostream& out);
