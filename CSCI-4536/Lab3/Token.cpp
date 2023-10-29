2d72517066b23f0dacc8b2a5d3d45b278d78186c
James Mok <hoodieman8@gmail.com>
2023-10-29 01:52:56 -0400

    style: Token.cpp print() now has ' ' to indicate start and end of char

--- CSCI-4536/Lab3/Token.cpp
+++ CSCI-4536/Lab3/Token.cpp
@@ -6,7 +6,7 @@ Token(string name, TokenType type) : name(name), type(type), refCount(1) {
 
 ostream& Token::
 print(ostream& out){
-    out << "Token Name: " << name << " | Type: " << TokenTypeName[type] << " | Times Referenced: " << refCount;
+    out << "Token Name: '" << name << "' | Type: " << TokenTypeName[type] << " | Times Referenced: " << refCount;
     return out;
 }
     
