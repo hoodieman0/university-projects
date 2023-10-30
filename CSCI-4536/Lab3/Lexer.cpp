78a8b9ab8ad7944c8fb2992c067c08a8e3a12d89
Drew <123031174+SnakeHeadTDT@users.noreply.github.com>
2023-10-29 21:09:59 -0400

    Update Lexer.cpp
    Added comments

--- CSCI-4536/Lab3/Lexer.cpp
+++ CSCI-4536/Lab3/Lexer.cpp
@@ -1,5 +1,6 @@
 #include "Lexer.hpp"
 
+//Throws errors if cant open file
 Lexer::
 Lexer(string filename) : state(STARTING), currentChar(' '), tokenString(""){
     inFile.open(filename);
@@ -15,6 +16,7 @@ Lexer(string filename) : state(STARTING), currentChar(' '), tokenString(""){
     outFile << "~Comments~\n" << endl;
 }
 
+//closes file when deconstructing
 Lexer::
 ~Lexer(){
     inFile.close();
@@ -30,6 +32,7 @@ isNumber(string s) const{
     return true;
 }
 
+//calls doLex on every character unless its a /n in which cases its eof
 void Lexer::
 doLex(){
     while (true){ 
@@ -39,14 +42,22 @@ doLex(){
             outFile << *this;
             return;
         }
+
+        //state is STARTING by default
+        //Depending on the input char read, the state changes
         switch(state){
             case STARTING:
                 doStart();
                 break;  
+
+            //checks for whitespace and changes state
             case SLASH_PENDING: // TODO make helper function
                 if (currentChar == ' ') state = ACQUIRING_SLASH;
                 else state = ACQUIRING_TOKEN;
                 break;
+
+            //Reads line to output file
+            //Changes state to start once it reaches a newline char
             case ACQUIRING_SLASH: // TODO make helper function
                 while (true){
                     if (currentChar == '\n') break;
@@ -56,10 +67,14 @@ doLex(){
                 outFile << currentChar; // adds the trailing \n
                 state = STARTING;
                 break;
+            
+            //changes state to acquirinParen or acquiringToken
             case PAREN_PENDING: // TODO make helper function
                 if (currentChar == ' ') state = ACQUIRING_PAREN;
                 else state = ACQUIRING_TOKEN;
                 break;
+
+            //Reads in char's till a ')' is found
             case ACQUIRING_PAREN: // TODO make helper function
                 while (true){
                     if (currentChar == ')') break;
@@ -68,6 +83,9 @@ doLex(){
                 }
                 outFile << endl; // adds a trailing \n
                 state = STARTING;
+
+            //main case, keeps adding to the token string until breaks
+            //checks if the tokenString is a number or word
             case ACQUIRING_TOKEN: // TODO make helper function
                 while (true){
                     if (currentChar == ' ' || currentChar == '\n' || currentChar == 13) break;
@@ -89,6 +107,8 @@ doLex(){
                 }
 
                 break;
+            
+            //Creates a tokenString
             case ACQUIRING_STRING: // TODO make helper function
                 tokenString = "";
                 while (true){
@@ -99,6 +119,7 @@ doLex(){
                 doToken(tokenString, STRING);
                 state = STARTING;
                 break;
+            
             case DONE:
             default:
                 break;
@@ -106,6 +127,7 @@ doLex(){
     }
 }
 
+//prints out map
 ostream& Lexer::
 print(ostream& out) const {
     out << "~Token Info~\n" << endl;
@@ -116,6 +138,7 @@ print(ostream& out) const {
     return out;
 }
 
+//searches the map to see if the Token already exists, if it does +1. If not, add to map
 void Lexer::
 doToken(string name, TokenType type){
      if (map<string, Token>::iterator search = tokenMap.find(name); 
@@ -130,6 +153,7 @@ doToken(string name, TokenType type){
     tokenString = "";
 }
 
+//Checks current char and changes state depending
 void Lexer::
 doStart(){
     switch(currentChar){
