f3ed03634ab4a955ffd080096695f9197b528532
hoodieman0 <hoodieman8@gmail.com>
2024-02-02 02:12:58 -0500

    feat: added initial outline for InputB() & InputC() unit tests

--- CSCI-4534/Homework-1/UnitTest-SDES.cpp
+++ CSCI-4534/Homework-1/UnitTest-SDES.cpp
@@ -22,6 +22,40 @@ int UnitTest_InputA(){
     return 0;
 }
 
+/// @brief check the SDES class against question 4b and verify correctness
+/// @return int 0 on pass, 1 on fail 
+int UnitTest_InputB(){
+    try{
+        const unsigned int plaintext = 0b01110111; 
+        const unsigned int key = 0b1101110111;
+        const unsigned int ciphertext = 0b01110101;
+
+        SDES cipher(key, true, true);
+        const unsigned int resultCipher = cipher.encrypt(plaintext);
+        if (resultCipher != ciphertext) throw "Returned cipher text is wrong!";
+
+        const unsigned int resultPlain = cipher.decrypt(resultCipher);
+        if (resultPlain != plaintext) throw "Returned plain text is wrong!";
+
+    } catch(const char* txt) {
+        cout << txt << endl; return 1;
+    } catch (...) { return 1; }
+
+    return 0;
+}
+
+/// @brief check the SDES class against question 4c and verify correctness
+/// @return int 0 on pass, 1 on fail 
+int UnitTest_InputC(){
+    try{
+        throw "Not implemented";
+    } catch(const char* txt) {
+        cout << txt << endl; return 1;
+    } catch (...) { return 1; }
+
+    return 0;
+}
+
 /// @brief check the SDES class against question 4a and verify correctness
 /// @return int 0 on pass, 1 on fail 
 int UnitTest_RunHomework(){
