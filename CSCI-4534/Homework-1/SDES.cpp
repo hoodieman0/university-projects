7a6fb9a15bc8b4c2fcd18494a382903bcfa17701
hoodieman0 <hoodieman8@gmail.com>
2024-02-01 21:41:36 -0500

    feat: added keyGen() outline
    and stylized constructor name

--- CSCI-4534/Homework-1/SDES.cpp
+++ CSCI-4534/Homework-1/SDES.cpp
@@ -1,5 +1,27 @@
 #include "SDES.hpp"
 
-SDES::SDES(const unsigned int key, bool output, bool verbose) : output(output), verbose(verbose) {
+SDES::
+SDES(const unsigned int key, bool output, bool verbose) : output(output), verbose(verbose) {
     keyGen(key);
+}
+
+void SDES::
+keyGen(const unsigned int key){
+    // P10 -> SHIFT -> P8 -> keyOne
+    // P10 -> SHIFT -> SHIFT-> P8 -> keyTwo
+    unsigned int resultP10 = permute10(key);
+
+    unsigned int left10 = resultP10 & LeftBits;
+    left10 = left10 >> 5;
+    left10 = leftShift(left10);
+
+    unsigned int right10 = leftShift(resultP10 & RightBits);
+    unsigned int combined = (left10 << 5) + right10;
+    keyOne = permute8(combined);
+
+    left10 = leftShift(left10);
+    right10 = leftShift(right10);
+    combined = (left10 << 5) + right10;
+
+    keyTwo = permute8(combined);
 }
