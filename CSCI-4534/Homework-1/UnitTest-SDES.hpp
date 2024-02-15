40e9420330b1dc6c159f85b75928146515b5e152
James Mok <hoodieman8@gmail.com>
2024-02-14 19:45:42 -0500

    docs: added function comments to UnitTest-SDES.hpp

--- CSCI-4534/Homework-1/UnitTest-SDES.hpp
+++ CSCI-4534/Homework-1/UnitTest-SDES.hpp
@@ -7,10 +7,34 @@
 
 using namespace std;
 
+/// ----------------------------------------------------------------------------
+/// @brief check the SDES class against question 4a and verify correctness
+/// @return int 0 on pass, 1 on fail 
+/// ----------------------------------------------------------------------------
 int UnitTest_InputA();
+
+/// ----------------------------------------------------------------------------
+/// @brief check the SDES class against question 4b and verify correctness
+/// @return int 0 on pass, 1 on fail 
+/// ----------------------------------------------------------------------------
 int UnitTest_InputB();
+
+/// ----------------------------------------------------------------------------
+/// @brief check the SDES class against question 4c and verify correctness
+/// @return int 0 on pass, 1 on fail 
+/// ----------------------------------------------------------------------------
 int UnitTest_InputC();
+
+/// ----------------------------------------------------------------------------
+/// @brief check the SDES class against question 4a and verify correctness
+/// @return int 0 on pass, 1 on fail 
+/// ----------------------------------------------------------------------------
 int UnitTest_RunHomework();
+
+/// ----------------------------------------------------------------------------
+/// @brief run all the unit tests for the SDES class
+/// @return int 0 on pass, 1 on fail 
+/// ----------------------------------------------------------------------------
 int UnitTest_RunAll();
 
 #endif
