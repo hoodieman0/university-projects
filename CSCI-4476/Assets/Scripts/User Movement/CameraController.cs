ec46d952f602a22ebe7b9f2e50ddcfca72830769
James Mok <hoodieman8@gmail.com>
2024-03-26 01:55:34 -0400

    refact: updated CameraController to have X and Y sensitivities

--- CSCI-4476/Assets/Scripts/User Movement/CameraController.cs
+++ CSCI-4476/Assets/Scripts/User Movement/CameraController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] float cameraSpeed = 2f;
+    [SerializeField] float cameraSpeedX = 2f;
+    [SerializeField] float cameraSpeedY = 2f;
     Vector2 newDir = Vector2.zero;
 
     void Start(){
@@ -16,8 +17,8 @@ public class CameraController : MonoBehaviour
 
     void MouseLook(Vector2 lookDir){
         newDir += lookDir;
-        transform.localRotation = Quaternion.Euler(Mathf.Clamp(-newDir.y * cameraSpeed, -90f, 90f), 0f, 0f);
-        transform.parent.localRotation = Quaternion.Euler(0f, newDir.x * cameraSpeed, 0f);
+        transform.localRotation = Quaternion.Euler(Mathf.Clamp(-newDir.y * cameraSpeedY, -90f, 90f), 0f, 0f);
+        transform.parent.localRotation = Quaternion.Euler(0f, newDir.x * cameraSpeedX, 0f);
     }
 
     void OnDisable() {
