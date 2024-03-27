a6a6a358a5b78aeb82735f3e547b611196377859
James Mok <hoodieman8@gmail.com>
2024-03-27 01:27:00 -0400

    feat: added draggable and grabbable to FirstPerson

--- CSCI-4476/Assets/Scripts/Interaction/Draggable.cs
+++ CSCI-4476/Assets/Scripts/Interaction/Draggable.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class Draggable : MonoBehaviour, IInteractable
 {
     [SerializeField] float breakForce = 100f;
--- CSCI-4476/Assets/Scripts/Interaction/Grabbable.cs
+++ CSCI-4476/Assets/Scripts/Interaction/Grabbable.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class Grabbable : MonoBehaviour, IInteractable
 {
     FixedJoint joint;
     [SerializeField] float breakForce = 100f;
     public void StartInteract(Interactor interactor){
+        Debug.Log("Grab");
         joint = gameObject.AddComponent<FixedJoint>();
         joint.connectedBody = interactor.GetComponent<Rigidbody>();
         joint.breakForce = breakForce;
@@ -15,6 +15,7 @@ public class Grabbable : MonoBehaviour, IInteractable
     }
 
     public void StopInteract(Interactor interactor){
+        Debug.Log("Drop");
         Destroy(joint);
     }
 }
