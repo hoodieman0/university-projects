f78459182ee6b8983b1589b0a4605f96f73f8dcb
James Mok <hoodieman8@gmail.com>
2024-03-27 02:25:31 -0400

    feat: added Door prefab and Opennable functionality

--- CSCI-4476/Assets/Scripts/Interaction/Opennable.cs
+++ CSCI-4476/Assets/Scripts/Interaction/Opennable.cs
@@ -7,9 +7,19 @@ public class Opennable : MonoBehaviour, IInteractable
 {
     [SerializeField] bool pushToOpen = true;
     [SerializeField] float maxRotation = 90f;
+    [SerializeField] float moveDuration = 1f;
+    float startingRotation;
+
+    void Start(){
+        startingRotation = transform.parent.rotation.y;
+        maxRotation = pushToOpen ? -maxRotation : maxRotation;
+    }
 
     public void StartInteract(Interactor interactor){
-        
+        if (!LeanTween.isTweening(transform.parent.gameObject)){
+            float target = transform.parent.rotation.y == startingRotation ? maxRotation : startingRotation;
+            LeanTween.rotateY(transform.parent.gameObject, target, moveDuration);
+        }
     }
 
     public void StopInteract(Interactor interactor){
