7f2a841e821bc754c42402c2cf2b30b90188b29c
James Mok <hoodieman8@gmail.com>
2024-04-16 19:00:58 -0400

    refact: made signHash() const

--- CSCI-4534/Homework-3/DSA.hpp
+++ CSCI-4534/Homework-3/DSA.hpp
@@ -18,7 +18,7 @@ class DSA {
 
     public:
     DSA(int p, int q, int h, int privateKey);
-    Signature signHash(int hash, int k);
+    Signature signHash(int hash, int k) const;
     bool verifyHash(const int hash, const Signature sig) const;
 
     ~DSA()=default;
