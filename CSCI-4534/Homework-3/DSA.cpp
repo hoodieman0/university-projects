e2a02e548a6cab677ea43d5f01441041996bdfa8
James Mok <hoodieman8@gmail.com>
2024-04-17 02:12:17 -0400

    docs: added comments to DSA class files

--- CSCI-4534/Homework-3/DSA.cpp
+++ CSCI-4534/Homework-3/DSA.cpp
@@ -1,10 +1,19 @@
 #include "DSA.hpp"
 
+/// ----------------------------------------------------------------------------
+/// @brief constructor for the DSA class, generates g & public key
+/// @param p (int) a prime number
+/// @param q (int) a factor of (p-1)
+/// @param h (int) a chosen number between 0 and p-1, and fastModExponentiation(h, (p-1)/q, p) > 1
+/// @param privateKey (int) a number between 0 and q
+/// @param verbose (bool) a flag to show debugging information
+/// @param output (bool) a flag to show homework outputs
+/// ----------------------------------------------------------------------------
 DSA::
 DSA(int p, int q, int h, int privateKey, bool verbose, bool output) :
 p(p), q(q), h(h), privateKey(privateKey), verbose(verbose), output(output) {
-    g = fastModExponentiation(h, (p-1)/q, p);
-    publicKey = fastModExponentiation(g, privateKey, p);
+    g = fastModExponentiation(h, (p-1)/q, p);               // h ^ ([p-1]/q) mod p
+    publicKey = fastModExponentiation(g, privateKey, p);    // g ^ x mod p
 
     if (verbose || output){
         cout << "g:             " << g << endl;
@@ -12,11 +21,17 @@ p(p), q(q), h(h), privateKey(privateKey), verbose(verbose), output(output) {
     }
 }
 
+/// ----------------------------------------------------------------------------
+/// @brief calcuates the signature values r & s for the given hash and random number
+/// @param hash (int) the hash of a message
+/// @param k (int) a random number < q
+/// @return (Signature) the r & s values for hash and k
+/// ----------------------------------------------------------------------------
 Signature DSA::
 signHash(const int hash, const int k) const{
-    int r = fastModExponentiation(g, k, p) % q;
-    int inverseK = modInverse(k, q);
-    int s = (inverseK * (hash + (privateKey * r))) % q; 
+    int r = fastModExponentiation(g, k, p) % q;             // (g ^ k mod p) mod q
+    int inverseK = modInverse(k, q);                        // k^-1 mod q
+    int s = (inverseK * (hash + (privateKey * r))) % q;     // [k^-1 (hash + privateKey * r)] mod q
 
     if (verbose || output){
         cout << "r:     " << r << endl;
@@ -29,6 +44,12 @@ signHash(const int hash, const int k) const{
     };
 }
 
+/// ----------------------------------------------------------------------------
+/// @brief verifies that the signature matches the given hash
+/// @param hash (int) the hash of a message
+/// @param sig (Signature) the r & s values for the signature
+/// @return (bool) true if the signature is paired with hash, otherwise false
+/// ----------------------------------------------------------------------------
 bool DSA::
 verifyHash(const int hash, const Signature sig) const{
     int w = modInverse(sig.s, q);
--- CSCI-4534/Homework-3/DSA.hpp
+++ CSCI-4534/Homework-3/DSA.hpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+// see powerpoint for naming convention
+
 struct Signature {
     int r;
     int s;
@@ -16,11 +18,34 @@ private:
     // privateKey = x, publicKey = y
     int p, q, h, privateKey;    // globally known
     int publicKey, g;           // derived
-    bool verbose, output;
+    bool verbose, output;       // output flags
 
 public:
+    /// ----------------------------------------------------------------------------
+    /// @brief constructor for the DSA class, generates g & public key
+    /// @param p (int) a prime number
+    /// @param q (int) a factor of (p-1)
+    /// @param h (int) a chosen number between 0 and p-1, and fastModExponentiation(h, (p-1)/q, p) > 1
+    /// @param privateKey (int) a number between 0 and q
+    /// @param verbose (bool) a flag to show debugging information
+    /// @param output (bool) a flag to show homework outputs
+    /// ----------------------------------------------------------------------------
     DSA(int p, int q, int h, int privateKey, bool verbose = false, bool output = false);
+    
+    /// ----------------------------------------------------------------------------
+    /// @brief calcuates the signature values r & s for the given hash and random number
+    /// @param hash (int) the hash of a message
+    /// @param k (int) a random number < q
+    /// @return (Signature) the r & s values for hash and k
+    /// ----------------------------------------------------------------------------
     Signature signHash(const int hash, const int k) const;
+
+    /// ----------------------------------------------------------------------------
+    /// @brief verifies that the signature matches the given hash
+    /// @param hash (int) the hash of a message
+    /// @param sig (Signature) the r & s values for the signature
+    /// @return (bool) true if the signature is paired with hash, otherwise false
+    /// ----------------------------------------------------------------------------
     bool verifyHash(const int hash, const Signature sig) const;
 
     ~DSA()=default;
