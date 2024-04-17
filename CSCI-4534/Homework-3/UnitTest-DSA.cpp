f802f4313398f4a35ca849a97e74ad8b73e5ee5f
James Mok <hoodieman8@gmail.com>
2024-04-16 23:57:20 -0400

    feat: defined UnitTest_InputA() and verified it runs as expected

--- CSCI-4534/Homework-3/UnitTest-DSA.cpp
+++ CSCI-4534/Homework-3/UnitTest-DSA.cpp
@@ -1,5 +1,49 @@
 #include "UnitTest-DSA.hpp"
 
+int UnitTest_InputA(){
+    try {
+        int p = 7,
+            q = 3,
+            h = 3,
+            x = 2,
+            k = 1,
+            HM1 = 3,
+            HM2 = 4;
+
+            DSA cipher(p, q, h, x, true, true);
+            cout << "----------------------------------------------------------------------------" << endl;
+            cout << "Sign Hash:" << endl;
+            const Signature sign = cipher.signHash(HM1, k);
+
+            if (sign.s != 1 || sign.r != 2) throw "Returned signature is wrong!";
+            
+            cout << "----------------------------------------------------------------------------" << endl;
+            cout << "Verify Real Hash" << endl;
+            bool goodVerification = cipher.verifyHash(HM1, sign);
+
+            if (!goodVerification) throw "Good verification went wrong!";
+
+            cout << "----------------------------------------------------------------------------" << endl;
+            cout << "Verify Fake Hash" << endl;
+            bool badVerification = cipher.verifyHash(HM2, sign);
+
+            if (badVerification) throw "Bad verification went wrong!";
+            cout << "----------------------------------------------------------------------------" << endl;
+    } catch(const char* txt) {
+        cout << txt << endl; return 1;
+    } catch (...) { return 1; }
+    
+    return 0;
+}
+
+int UnitTest_InputB(){
+    return 0;
+}
+
+int UnitTest_InputC(){
+    return 0;
+}
+
 /// ----------------------------------------------------------------------------
 /// @brief run all the unit tests for the SDES class
 /// @return int 0 on pass, 1 on fail 
