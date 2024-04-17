d45c3ad745b8d78249ed14c7b6bcf60d0691cbea
James Mok <hoodieman8@gmail.com>
2024-04-16 23:56:43 -0400

    feat: added verbose and homework outputs to DSA class

--- CSCI-4534/Homework-3/DSA.cpp
+++ CSCI-4534/Homework-3/DSA.cpp
@@ -1,10 +1,15 @@
 #include "DSA.hpp"
 
 DSA::
-DSA(int p, int q, int h, int privateKey) :
-p(p), q(q), h(h), privateKey(privateKey) {
+DSA(int p, int q, int h, int privateKey, bool verbose, bool output) :
+p(p), q(q), h(h), privateKey(privateKey), verbose(verbose), output(output) {
     g = fastModExponentiation(h, (p-1)/q, p);
     publicKey = fastModExponentiation(g, privateKey, p);
+
+    if (verbose || output){
+        cout << "g:             " << g << endl;
+        cout << "publicKey:     " << publicKey << endl;
+    }
 }
 
 Signature DSA::
@@ -12,6 +17,12 @@ signHash(const int hash, const int k) const{
     int r = fastModExponentiation(g, k, p) % q;
     int inverseK = modInverse(k, q);
     int s = (inverseK * (hash + (privateKey * r))) % q; 
+
+    if (verbose || output){
+        cout << "r:     " << r << endl;
+        cout << "s:     " << s << endl;
+    }
+
     return Signature {
         r,
         s
@@ -24,5 +35,13 @@ verifyHash(const int hash, const Signature sig) const{
     int u1 = (hash * w) % q;
     int u2 = (sig.r * w) % q;
     int v = (fastModExponentiation(g, u1, p) * fastModExponentiation(publicKey, u2, p)) % q;
+
+    if (verbose || output){
+        cout << "w:         " << w << endl;
+        cout << "u1:        " << u1 << endl;
+        cout << "u2:        " << u2 << endl;
+        cout << "v:         " << v << endl;
+        cout << "v == r:    " << boolalpha << (v == sig.r) << endl;
+    }
     return v == sig.r;
 }
--- CSCI-4534/Homework-3/DSA.hpp
+++ CSCI-4534/Homework-3/DSA.hpp
@@ -1,6 +1,7 @@
 #ifndef DSA_HPP
 #define DSA_HPP
 
+#include <iostream>
 #include "Cryptography.hpp"
 
 using namespace std;
@@ -11,13 +12,14 @@ struct Signature {
 };
 
 class DSA {
-    private:
+private:
     // privateKey = x, publicKey = y
     int p, q, h, privateKey;    // globally known
     int publicKey, g;           // derived
+    bool verbose, output;
 
-    public:
-    DSA(int p, int q, int h, int privateKey);
+public:
+    DSA(int p, int q, int h, int privateKey, bool verbose = false, bool output = false);
     Signature signHash(const int hash, const int k) const;
     bool verifyHash(const int hash, const Signature sig) const;
 
