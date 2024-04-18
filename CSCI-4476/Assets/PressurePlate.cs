420f990d8d7054aaa442336b04c168e6ef23fb58
James Mok <hoodieman8@gmail.com>
2024-04-17 22:57:03 -0400

    feat: added pressure plate with interaction

---
+++ CSCI-4476/Assets/PressurePlate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] Transform button;
+    [SerializeField] float yMin = 0.1f;
+    float yMax; // set in editor by object
+    [SerializeField] bool pressed = false;
+    [SerializeField] GameObject target;
+
+    void Start(){
+        yMax = button.localPosition.y;
+    }
+
+    void Update(){
+        if (button.localPosition.y < yMin){
+            button.localPosition = new(0f, yMin, 0f);
+        }
+        else if (button.localPosition.y > yMax){
+            button.localPosition = new(0f, yMax, 0f);
+        }
+        else if (button.localPosition.y == yMin && !pressed){
+            pressed = true;
+            target.GetComponent<IInteractable>()?.StartInteract();
+            Debug.Log("Pressed");
+        }
+        else if (pressed && button.localPosition.y != yMin){
+            target.GetComponent<IInteractable>()?.StopInteract();
+            pressed = false;
+            Debug.Log("Unpressed");
+        }
+    }
+}
